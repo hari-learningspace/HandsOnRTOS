e5620b2f015acac67d72a1e6fc32eb8c244b370e
Hari Udayakumar <hari.udaykumar@continental-corporation.com>
2020-08-28 04:22:47 +0800

    STM32 FreeRtos project with SystemView Configuration

---
+++ stm32_free_rtos/Core/Src/freertos.c
@@ -0,0 +1,237 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "SEGGER_SYSVIEW.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 128 * 4
+};
+/* Definitions for Green_Task */
+osThreadId_t Green_TaskHandle;
+uint32_t GreenTaskBuffer[ 128 ];
+osStaticThreadDef_t GreenTaskControlBlock;
+const osThreadAttr_t Green_Task_attributes = {
+  .name = "Green_Task",
+  .stack_mem = &GreenTaskBuffer[0],
+  .stack_size = sizeof(GreenTaskBuffer),
+  .cb_mem = &GreenTaskControlBlock,
+  .cb_size = sizeof(GreenTaskControlBlock),
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for myTask03 */
+osThreadId_t myTask03Handle;
+uint32_t myTask03Buffer[ 128 ];
+osStaticThreadDef_t myTask03ControlBlock;
+const osThreadAttr_t myTask03_attributes = {
+  .name = "myTask03",
+  .stack_mem = &myTask03Buffer[0],
+  .stack_size = sizeof(myTask03Buffer),
+  .cb_mem = &myTask03ControlBlock,
+  .cb_size = sizeof(myTask03ControlBlock),
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for myTask04 */
+osThreadId_t myTask04Handle;
+uint32_t myTask04Buffer[ 128 ];
+osStaticThreadDef_t myTask04ControlBlock;
+const osThreadAttr_t myTask04_attributes = {
+  .name = "myTask04",
+  .stack_mem = &myTask04Buffer[0],
+  .stack_size = sizeof(myTask04Buffer),
+  .cb_mem = &myTask04ControlBlock,
+  .cb_size = sizeof(myTask04ControlBlock),
+  .priority = (osPriority_t) osPriorityHigh,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void GreenTask(void *argument);
+void StartTask03(void *argument);
+void StartTask04(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of Green_Task */
+  Green_TaskHandle = osThreadNew(GreenTask, NULL, &Green_Task_attributes);
+
+  /* creation of myTask03 */
+  myTask03Handle = osThreadNew(StartTask03, NULL, &myTask03_attributes);
+
+  /* creation of myTask04 */
+  myTask04Handle = osThreadNew(StartTask04, NULL, &myTask04_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  SEGGER_SYSVIEW_PrintfHost("Default Task");
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_GreenTask */
+/**
+* @brief Function implementing the Green_Task thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_GreenTask */
+void GreenTask(void *argument)
+{
+  /* USER CODE BEGIN GreenTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  SEGGER_SYSVIEW_PrintfHost("GreenLed Task - Low Priority");
+      HAL_GPIO_TogglePin(GREENLED_GPIO_Port,GREENLED_Pin);
+	  osDelay(3);
+  }
+  /* USER CODE END GreenTask */
+}
+
+/* USER CODE BEGIN Header_StartTask03 */
+/**
+* @brief Function implementing the myTask03 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask03 */
+void StartTask03(void *argument)
+{
+  /* USER CODE BEGIN StartTask03 */
+  /* Infinite loop */
+  for(;;)
+  {
+	  SEGGER_SYSVIEW_PrintfHost("Task3 - Normal Priority");
+	  HAL_GPIO_TogglePin(LED1_GPIO_Port,LED1_Pin);
+	  osDelay(5);
+  }
+  /* USER CODE END StartTask03 */
+}
+
+/* USER CODE BEGIN Header_StartTask04 */
+/**
+* @brief Function implementing the myTask04 thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask04 */
+void StartTask04(void *argument)
+{
+  /* USER CODE BEGIN StartTask04 */
+  /* Infinite loop */
+  for(;;)
+  {
+	  SEGGER_SYSVIEW_PrintfHost("Task 3 - High Priority");
+	  HAL_GPIO_TogglePin(LED2_GPIO_Port,LED2_Pin);
+	  osDelay(10);
+  }
+  /* USER CODE END StartTask04 */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
